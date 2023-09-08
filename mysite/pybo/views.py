616c0e7308612552df3c79dc3ccdfa547346d221
Jeongbeen_Son <jeongbeenson19@gmail.com>
2023-09-08 17:27:09 +0900

    23.9.8

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     """
@@ -24,8 +25,28 @@ def answer_create(request, question_id):
     pybo 답변 등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'),
-                               create_date=timezone.now())
-    return redirect('pybo:detail', question_id=question_id)
-
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo/question_detail.html', context)
+        
+def question_create(request):
+    """
+    pybo 질문 등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question_create.date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
 # Create your views here.
