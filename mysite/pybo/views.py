4bcdc083f6ff60d799d7fbc437a4fa6114b75772
Jeongbeen_Son <jeongbeenson19@gmail.com>
2023-09-14 17:47:29 +0900

    230914

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -3,13 +3,23 @@ from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 def index(request):
     """
     pybo 목록 출력
     """
+    # 입력 인자
+    page = request.GET.get('page', '1') # 페이지
+
+    # 조회
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+
+    # 페이징 처리
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여 주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
@@ -32,8 +42,8 @@ def answer_create(request, question_id):
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pybo/question_detail.html', context)
-        
+            return redirect('pybo/question_detail.html', question_id=question_id)
+          
 def question_create(request):
     """
     pybo 질문 등록
