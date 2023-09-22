9e9999191b77cc9f3421b5d56189ab9113b20ebc
Jeongbeen_Son <jeongbeenson19@gmail.com>
2023-09-22 17:02:58 +0900

    230922

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from pybo import views
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
-    path('', views.index, name='index'), # / 페이지에 해당하는 urlpatterns
+    path('', base_views.index, name='index'), # / 페이지에 해당하는 urlpatterns
 ]
---
+++ mysite/pybo/views/base_views.py
@@ -0,0 +1,30 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+
+from ..models import Question
+
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    # 입력 인자
+    page = request.GET.get('page', '1') # 페이지
+
+    # 조회
+    question_list = Question.objects.order_by('-create_date')
+
+    # 페이징 처리
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여 주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
