5633704fbcd82a4ef393db57d7a4dcce7dbef77f
Jeongbeen_Son <jeongbeenson19@gmail.com>
2023-09-26 17:26:24 +0900

    230926

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
@@ -10,15 +11,23 @@ def index(request):
     """
     # 입력 인자
     page = request.GET.get('page', '1') # 페이지
+    kw = request.GET.get('kw', '') # 검색어
 
     # 조회
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontations=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__usernmae__icontains=kw)
+        ).distinct()
 
     # 페이징 처리
     paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여 주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
