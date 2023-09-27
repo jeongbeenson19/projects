31651eb9260dbae37ff4ed38d1a4775b663bb63a
Jeongbeen_Son <jeongbeenson19@gmail.com>
2023-09-27 16:41:19 +0900

    230927

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from ..models import Question
 
@@ -12,15 +12,26 @@ def index(request):
     # 입력 인자
     page = request.GET.get('page', '1') # 페이지
     kw = request.GET.get('kw', '') # 검색어
+    so = request.GET.get('so', 'recent') # 정렬 기준
 
+    # 정렬
+
+    if so == 'recommend':
+        question_list = Question.objects.annotate(
+            num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(
+        num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else: # recent
+        question_list = Question.objects.order_by('-create_date')
+    
     # 조회
-    question_list = Question.objects.order_by('-create_date')
     if kw:
         question_list = question_list.filter(
             Q(subject__icontains=kw) |
-            Q(content__icontations=kw) |
+            Q(content__icontains=kw) |
             Q(author__username__icontains=kw) |
-            Q(answer__author__usernmae__icontains=kw)
+            Q(answer__author__username__icontains=kw)
         ).distinct()
 
     # 페이징 처리
