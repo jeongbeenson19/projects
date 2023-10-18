a9300b5ae4e1a68b1e441c18cd4c902438117998
Jeongbeen_Son <jeongbeenson19@gmail.com>
2023-10-18 17:00:34 +0900

    question_view에 category_name 전달 오류 해결

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -5,13 +5,12 @@ app_name = 'pybo'
 
 urlpatterns = [
    #base_views.py
-   path('question/list/', base_views.index, name='index'),
+   path('question/list/',base_views.index, name='index'),
    path('quesiton/list/<str:category_name>/', base_views.index, name='index'),
    path('question/detail/<int:question_id>/', base_views.detail, name='detail'),
 
    #question_views.py
-   path('question/create/',
-     question_views.question_create, name='question_create'),
+   path('question/create/', question_views.question_create, name='question_create'),
    path('question/modify/<int:question_id>/',
      question_views.question_modify, name='question_modify'),
    path('question/delete/<int:question_id>/',
